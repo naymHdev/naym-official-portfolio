07a5fdd8e5960ab0141cd43132037a0f6946b28e
MacBook Pro <naym100m@gmail.com>
2024-05-13 21:04:49 +0600

    update a contact page create compleate

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -1,11 +1,32 @@
+import bg from "../../../Public/background/contact-background.png";
+import Form from "./Form";
 
+export default function Contact() {
+  return (
+    <>
+      <img
+        src={bg}
+        alt="Next.js Portfolio website's contact page background image"
+        sizes="100vw"
+        className="-z-50 fixed top-0 left-0 w-full h-full object-cover object-center"
+      />
 
-const Contact = () => {
-    return (
-        <div>
-            
+      <article className="relative w-full min-h-screen flex flex-col items-center justify-center py-8 sm:py-0 space-y-8">
+        <div className="flex flex-col items-center justify-center space-y-6 w-full sm:w-3/4">
+          <h1 className="text-accent font-semibold text-center text-4xl capitalize">
+            summon the wizard
+          </h1>
+          <p className="text-center font-light text-sm xs:text-base text-foreground">
+            Step into the circle of enchantment and weave your words into the
+            fabric of the cosmos. Whether you seek to conjure collaborations,
+            unlock mysteries, or simply share tales of adventure, your messages
+            are treasured scrolls within this realm. Use the form below to send
+            your missives through the ethereal network, and await the whisper of
+            magic in response.
+          </p>
         </div>
-    );
-};
-
-export default Contact;
+        <Form />
+      </article>
+    </>
+  );
+}

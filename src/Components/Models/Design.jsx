9e796fc84abdaaedf99743803ca6cc0430dd9d7d
MacBook Pro <naym100m@gmail.com>
2024-05-17 19:21:53 +0600

    modified the design page

--- src/Components/Models/Design.jsx
+++ src/Components/Models/Design.jsx
@@ -3,11 +3,13 @@ import { useGLTF } from "@react-three/drei";
 import { useFrame } from "@react-three/fiber";
 
 const Design = React.memo(function Staff(props) {
-  const { nodes, materials } = useGLTF("Public/models/staff-transformed.glb");
+  const { nodes, materials } = useGLTF("/models/staff-transformed.glb");
   const modelRef = useRef();
 
   useFrame(() => {
-    modelRef.current.rotation.y += 0.007;
+    if (modelRef.current) {
+      modelRef.current.rotation.y += 0.007;
+    }
   });
 
   return (
@@ -18,54 +20,59 @@ const Design = React.memo(function Staff(props) {
       position={[0, -2, 0]}
       ref={modelRef}
     >
-      <mesh
-        castShadow
-        receiveShadow
-        geometry={nodes.Wizard_Staff3_Wizard_Staff3_0.geometry}
-        material={materials.Wizard_Staff3}
-        position={[-0.041, 0.983, 0.768]}
-        rotation={[0, Math.PI / 2, 0]}
-        scale={0.04}
-      />
-      <mesh
-        castShadow
-        receiveShadow
-        geometry={nodes.Wizard_Staff3_Wizard_Staff3_0_1.geometry}
-        material={materials.Wizard_Staff3}
-        position={[-0.041, 0.983, 0.768]}
-        rotation={[0, Math.PI / 2, 0]}
-        scale={0.04}
-      />
-      <mesh
-        castShadow
-        receiveShadow
-        geometry={nodes.Wizard_Staff3_Wizard_Staff3_0_2.geometry}
-        material={materials.Wizard_Staff3}
-        position={[-0.041, 0.983, 0.768]}
-        rotation={[0, Math.PI / 2, 0]}
-        scale={0.04}
-      />
-      <mesh
-        castShadow
-        receiveShadow
-        geometry={nodes.Wizard_Staff3_Wizard_Staff3_0_3.geometry}
-        material={materials.Wizard_Staff3}
-        position={[-0.041, 0.983, 0.768]}
-        rotation={[0, Math.PI / 2, 0]}
-        scale={0.04}
-      />
-      <mesh
-        castShadow
-        receiveShadow
-        geometry={nodes.Wizard_Staff2_Wizard_Staff2_0.geometry}
-        material={materials.Wizard_Staff2}
-        position={[-0.041, 0.983, 0.768]}
-        rotation={[0, Math.PI / 2, 0]}
-        scale={0.04}
-      />
+      {nodes && materials && (
+        <>
+          <mesh
+            castShadow
+            receiveShadow
+            geometry={nodes.Wizard_Staff3_Wizard_Staff3_0.geometry}
+            material={materials.Wizard_Staff3}
+            position={[-0.041, 0.983, 0.768]}
+            rotation={[0, Math.PI / 2, 0]}
+            scale={0.04}
+          />
+          <mesh
+            castShadow
+            receiveShadow
+            geometry={nodes.Wizard_Staff3_Wizard_Staff3_0_1.geometry}
+            material={materials.Wizard_Staff3}
+            position={[-0.041, 0.983, 0.768]}
+            rotation={[0, Math.PI / 2, 0]}
+            scale={0.04}
+          />
+          <mesh
+            castShadow
+            receiveShadow
+            geometry={nodes.Wizard_Staff3_Wizard_Staff3_0_2.geometry}
+            material={materials.Wizard_Staff3}
+            position={[-0.041, 0.983, 0.768]}
+            rotation={[0, Math.PI / 2, 0]}
+            scale={0.04}
+          />
+          <mesh
+            castShadow
+            receiveShadow
+            geometry={nodes.Wizard_Staff3_Wizard_Staff3_0_3.geometry}
+            material={materials.Wizard_Staff3}
+            position={[-0.041, 0.983, 0.768]}
+            rotation={[0, Math.PI / 2, 0]}
+            scale={0.04}
+          />
+          <mesh
+            castShadow
+            receiveShadow
+            geometry={nodes.Wizard_Staff2_Wizard_Staff2_0.geometry}
+            material={materials.Wizard_Staff2}
+            position={[-0.041, 0.983, 0.768]}
+            rotation={[0, Math.PI / 2, 0]}
+            scale={0.04}
+          />
+        </>
+      )}
     </group>
   );
 });
 
 export default Design;
+
 useGLTF.preload("/models/staff-transformed.glb");

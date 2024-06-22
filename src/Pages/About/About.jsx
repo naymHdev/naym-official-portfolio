448834c2d68b56916d23be71b8c83432c0e969aa
MacBook Pro <naym100m@gmail.com>
2024-06-22 16:36:15 +0600

    updated a about page

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -37,7 +37,7 @@ export default function About() {
       </div>
       <div className="relative w-full h-screen flex flex-col items-center justify-center mt-24">
         <div className="absolute flex flex-col items-center text-center top-1/2 sm:top-[60%] left-1/2 -translate-y-1/2 -translate-x-1/2">
-          <h1 className="font-bold text-6xl xs:text-7xl sm:text-8xl lg:text-9xl text-accent">
+          <h1 className="font-bold text-2xl xs:text-6xl lg:text-7xl text-accent">
             Naym Hossen
           </h1>
           <p className="font-light md:font-medium text-foreground text-lg">
